190190115450d355d9a9148a7533fdaa225c25d8
nickklosterman <nick_klosterman@yahoo.com>
2012-12-07 18:52:17 -0500

    almost there...

--- TemplateWriter.py
+++ TemplateWriter.py
@@ -3,6 +3,14 @@
 #http://www.gossamer-threads.com/lists/python/dev/662491
 from __future__ import with_statement
 
+import itertools
+
+#http://stackoverflow.com/questions/434287/what-is-the-most-pythonic-way-to-iterate-over-a-list-in-chunks
+#http://docs.python.org/3.3/library/itertools.html#itertools-recipes
+def grouper(iterable, chunksize, fillvalue=None):
+    args = [iter(iterable)] * chunksize
+    return itertools.zip_longest(*args, fillvalue=fillvalue)   #not sure why but I couldn't call zip_longest wo specifying itertools.zip_tools
+
 class SVGTemplateWriter:
     def __init__(self,addressfile,svgtemplate,keyword):
         self.addressfile=addressfile
@@ -13,20 +21,63 @@ class SVGTemplateWriter:
         self.incrementCounter=0    
         self.ExtractAddressList()
         #self.Temp()
-        self.CompressList() #this is the simple lazy way to move through the list
-#        self.CyclicSearchReplace()
-        self.SplitTemplateFile()
+        self.CompressList() #this is so later on we can use the simple lazy way to move through the list
+#        self.CyclicSearchReplace() # this was for a less efficient method where I would repeatedly loop through reading the template file and outputting code adn doing a search/replae on text that I needed to output.
+        self.SplitTemplateFile() #partition the template around the positions where we will insert our adddress text
+#        (grouper(self.addressList,len(self.templateList),"BLANK"))
+        self.ChunkAddressListForTemplate()
+
+    def ChunkAddressListForTemplate(self):
+        self.addressList.reverse # so we can pop from 'front' ....don't need to use this if not using pop and append
+        while len(self.addressList)>0:
+            temp=[]
+            for i in range(len(self.templateList)):
+                temp.append(self.addressList.pop)
+            self.OutputTemplateFile(temp)
+            
+    def OutputTemplateFile(self,addresslist):
+        tempTemplate=self.templateList#.reverse
+        tempTemplate.reverse
+        output=""
+        counter=0
+# something here is broke.....:(
+#         while len(tempTemplate)>0 or len(addresslist)>0:
+#             if len(tempTemplate)>0:
+#                 output+=str(tempTemplate.pop)
+#             if len(addresslist)>0:
+#                 output+=str(addresslist.pop)
+#             counter+=1
+#             if counter>10:
+#                 break
+        print(output)
+
+    def WriteOutput(self):
+        templateListLength=len(self.templateList)-1 #number of fields we can place in one output file
+        addressListLength=len(self.addressList)
+        blanks=addressListLength % templateListLength
+        #if we won't fit perfectly into the template, pad our addresslist
+        if addressListLength>templateListLength and blanks!=0:
+            for x in range(blanks):
+                self.addressList.append("BLANK")
+
+        for num in range(templateListLength):
+            output=self.templateList[0]
+
 
     def ExtractAddressList(self):
 #        print("using : %s" % self.addressfile)
         with open(self.addressfile,"r") as f:
             addresses = [elem for elem in f.read().split('\n') if elem]
             for address in addresses:
-                temp=address.split(';')
-                temp=[x.strip() for x in temp] #strip in lists http://stackoverflow.com/questions/7984169/using-strip-on-lists
-                self.addressList.append(temp) #whitespace automatically stripped. This may not be the desired behavior for some people. They may try to pad the output using spaces.
+                address_=address.strip(' \t\r\n')
+                if len(address_)>1 and address_[0]!='#': #skip comment lines
+                    temp=address.split(';')
+                    temp=[x.strip() for x in temp] #strip in lists http://stackoverflow.com/questions/7984169/using-strip-on-lists
+                    self.addressList.append(temp) #whitespace automatically stripped. This may not be the desired behavior for some people. They may try to pad the output using spaces.
         f.close()
 #        print(self.addressList)
+        # for item in self.addressList:
+        #     print(item)
 
     def Temp(self):
         for item in self.addressList:
@@ -128,6 +179,8 @@ if __name__=="__main__":
 
 #Must avoid keyword being reserved word in SVG
 #make sure no add'l spaces in your template, addresslist as this'll affect the balance of the layout. You may not notice it right away but you'll just feel that something is slightly off not knowing that an invisible space is throwing things off. 
+#padding with spaces in the addresslist file won't work as the whitespace will be stripped.
+#; is to be used as the delimiter between fields in the address. You could also just put each element on a separate line (as long as we continue to compact the list)
 
 #inkscape must be installed for (batch) printing which reqires exporting file to another format:http://unix.stackexchange.com/questions/11973/print-an-svg-from-the-command-line
 # inkscape --without-gui --export-pdf=foo.pdf foo.svg
